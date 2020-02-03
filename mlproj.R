04a884932e3b080e85336eecd53af562ff259d12
Marco Ripamonti <m.ripamonti@campus.unimib.it>
2020-02-03 12:11:50 +0100

    Initial plotting and settings of dataset

---
+++ mlproj.R
@@ -0,0 +1,27 @@
+library("ggplot2")
+library("corrplot")
+
+
+dataset = read.csv("stars.csv")
+colnames(dataset) = c("Temp", "Lum", "Rad", "AbsMagn", "Type", "Color", "SpectrClass")
+dataset = dataset[, c(1,2,3,4,6,7,5)]
+dataset$Type = factor(dataset$Type)
+types = c("BrownDwarf", "RedDwarf", "WhiteDwarf","MainSequence", "Supergiant", "Hypergiant")
+dataset$Type = factor(sapply(dataset$Type, function (x) { types[x]}))
+
+dataset.notarget = function(data) {
+  return(data[1:length(data)-1])
+}
+
+#check distribution of target
+targetTable = table(dataset$Type)
+pie(targetTable)
+plot(dataset, col = dataset$Type)
+
+# cor
+dataset.cor = cor(dataset.notarget(dataset[, 1:5]))
+dataset.cor.plot = corrplot(dataset.cor)
+
+ggplot(dataset, aes(x = Temp, y = AbsMagn, color = Type)) + geom_point()
+ggplot(dataset, aes(x = Lum, y = AbsMagn, color = Type)) + geom_point()
+ggplot(dataset, aes(x = Rad, y = AbsMagn, color = Type)) + geom_point()
